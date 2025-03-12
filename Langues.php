2c80870966ed56b835e183db46319749060c1ba8
Josue4231 <josuekialengela@gmail.com>
2025-03-12 08:17:01 +0100

    Add files via upload

---
+++ Langues.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Home Page
+ *
+ * PHP version 7
+ *
+ * @category  Page
+ * @package   Application
+ * @author    SIO-SLAM <sio@ldv-melun.org>
+ * @copyright 2019-2021 SIO-SLAM
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link      https://github.com/sio-melun/geoworld
+ */
+
+?>
+<?php  require_once 'header.php'; ?>
+<?php
+require_once 'inc/manager-db.php';
+
+$Langues = {};
+$desPays = getLanguageByContinent($Langues);
+?>
+
+<main role="main" class="flex-shrink-0">
